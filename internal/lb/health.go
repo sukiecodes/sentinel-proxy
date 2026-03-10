a5f58040e6a7a14b6361fd0c3bd19e5a92af37b4
Sukie Zhang <sukiezhang.sz@gmail.com>
2026-03-10 00:18:21 -0400

    added health.go logic, where there's verification if a backend is capable of handling user's traffic before proxy tries sending a user there

--- internal/lb/health.go
+++ internal/lb/health.go
@@ -1,1 +1,55 @@
+// Will verify that a backend is capable of handling user's traffic before proxy tries sending user there
 package lb
+
+import (
+	"net/http"
+	"fmt"
+	"time"
+	"log"
+)
+
+// Ping function takes in a backend server and returns true if able to connect
+func Ping(b *Backend) bool {
+	// defining a timeout so no deadlock
+	timeout := 2 * time.Second
+	client := http.Client {
+		Timeout: timeout,
+	}
+
+	resp, err := client.Get(b.URL.String())
+
+	// checking if there's a network error
+	if err != nil {
+		fmt.Println("Error trying to connect: ", err)
+		return false
+	} 
+
+	if resp.StatusCode != http.StatusOK {
+		log.Printf("Ping failed for %s: Status %d", b.URL, resp.StatusCode)
+		return false
+	}
+
+	// clean up response body immediately
+	defer resp.Body.Close()
+
+	return true 
+}
+
+func (s *ServerPool) HealthCheck() {
+	// ticker that fires every 10 seconds
+	ticker := time.NewTicker(10 * time.Second)
+
+	// looping forever, waiting for the tick
+	for range ticker.C {
+		log.Println("Starting active health check...")
+		s.checkBackends()
+	}
+}
+
+func (s *ServerPool) checkBackends() {
+	for _, b := range s.Backends {
+		status := Ping(b)
+		b.SetAlive(status)
+		log.Printf("Backend %s is alive: %v", b.URL, status)
+	}
+}
